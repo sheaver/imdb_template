60afa6613b8d52d41873f47c56122d4089cb4220
Raghu Betina <raghu@firstdraft.com>
2017-11-21 18:07:58 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "investments#index"
   # Routes for the Investment resource:
   # CREATE
